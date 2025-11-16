32ea6a386f03b96a37b9c67a2815a1e9fa245a65
ft5000 <fabian.tjernstrom@gmail.com>
2025-11-16 22:40:08 +0100

    image-crawl extends grid-view

--- src/app/components/image-crawl-view/image-crawl-view.ts
+++ src/app/components/image-crawl-view/image-crawl-view.ts
@@ -4,6 +4,7 @@ import { SanityService } from '../../services/sanity';
 import { CommonModule } from '@angular/common';
 import { GridItem } from '../grid-item/grid-item';
 import { VideoContainer } from '../video-container/video-container';
+import { GridView } from '../../classes/grid-view';
 
 @Component({
   selector: 'app-image-crawl-view',
@@ -11,37 +12,22 @@ import { VideoContainer } from '../video-container/video-container';
   templateUrl: './image-crawl-view.html',
   styleUrl: './image-crawl-view.scss',
 })
-export class ImageCrawlView {
+export class ImageCrawlView extends GridView {
   public images: ImageDTO[] = [];
 
   mo: MutationObserver | null = null;
 
   constructor(private sanity: SanityService) {
+    super();
   }
 
   ngOnInit() {
     this.sanity.getAllImages().subscribe((data: ImageDTO[]) => {
       this.images = data;
-      this.setupImageObserver();
+      this.setupItemObserver();
     });
 
     document.body.style.setProperty('--fg-color', 'white');
     document.body.style.setProperty('--bg-color', 'black');
-  }
-
-  private setupImageObserver() {
-    setTimeout(() => {
-      const images = document.querySelectorAll('.grid-item');
-      const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            (entry.target as HTMLElement).classList.add('show');
-            observer.unobserve(entry.target);
-          }
-        });
-      }, { threshold: 0.1 });
-
-      images.forEach(img => observer.observe(img));
-    });
   }
 }
